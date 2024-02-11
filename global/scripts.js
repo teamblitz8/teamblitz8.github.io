644155df69ab0f8a897ec2e29ec4009a155c3ec6
cheddar-cheese1234 <chen.wang88@icloud.com>
2024-02-11 21:30:04 +0000

    Homepage complete!
    Still have to replace pictures tho

--- global/scripts.js
+++ global/scripts.js
@@ -5,4 +5,10 @@ function hamburgerify() {
     } else {
       x.className = "topnav";
     }
-  }
+}
+
+(function(){
+  var i,e,d=document,s="script";i=d.createElement("script");i.async=1;i.charset="UTF-8";
+  i.src="https://cdn.curator.io/published/41ac22fb-1cfa-42ef-9b66-90aca1f9988d.js";
+  e=d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+  })();

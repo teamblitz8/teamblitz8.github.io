b86b41fc154385587967a34d6fa9d146052a9c1c
cheddar-cheese1234 <chen.wang88@icloud.com>
2026-03-26 22:18:58 +0000

    we try again

--- js/global.js
+++ js/global.js
@@ -1,16 +1,3 @@
-window.onload = (e) => {
-    let images = document.getElementsByClassName("img");
-    for (let i of images) {
-        let posOrNeg = "";
-        if (Math.random() > 0.5) {
-            posOrNeg = "-"
-        }
-        const para = `rotate(${posOrNeg}${Math.floor(Math.random() * 10)}deg)`
-        i.style.transform = para;
-        i.parentElement.style.transform = para;
-    }
-}
-
 const phone = () => {
     let nav = document.getElementById("nav");
 
@@ -19,20 +6,5 @@ const phone = () => {
     }
     else {
         nav.className = "phone";
-    }
-}
-
-const flick = (c) => {
-    let list = document.getElementsByClassName(c);
-    for (let i of list) {
-        if (i.className.includes("top")) {
-            i.className = `bottom ${c} img`;
-        }
-        else if (i.className.includes("middle")) {
-            i.className = `top ${c} img`
-        }
-        else if (i.className.includes("bottom")) {
-            i.className = `middle ${c} img`
-        }
     }
 }
